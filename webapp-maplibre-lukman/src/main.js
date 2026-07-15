1a3285fdcc6d0d396f08f886a674d8ecc9dbad66
Lukman Hakim <lukmansudasrii@gmail.com>
2026-07-15 23:25:01 +0700

    add raster to webMap

--- webapp-maplibre-lukman/src/main.js
+++ webapp-maplibre-lukman/src/main.js
@@ -34,6 +34,8 @@ const map = new Map({
 // }
 
 map.on('load', () => {
+
+  // layer vektor titik
   map.addSource('kota', {
   type: "geojson",
   data: naturalEarthData
@@ -51,6 +53,7 @@ map.addLayer({
   }
 })
 
+// layer vektor polygon
 map.addSource('pulau', {
   type: "geojson",
   data: areaData
@@ -68,5 +71,22 @@ map.addLayer({
   }
 });
 
+// layer raster
+map.addSource('patrick', {
+  type: 'image',
+  url: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ9voz-syNvxLl8OpsgnOtz_7WCNNiPCl5bI7RHgOrJNA&s=10',
+  coordinates: [
+   [79.16,-0.40], //top left
+   [94.18,-1.62],//top right
+   [94.65,-14.73], //bottom right
+   [72.97,-13.74] //bottom left
+  ]
+})
+
+map.addLayer({
+  id: 'patrick-image',
+  type: 'raster',
+  source: 'patrick',
+})
 
 });
